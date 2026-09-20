821b1d2032e7cf582145ca8a8137f07985ec1b04
Carlos Aganzo <carlos.aganzo@n-able.com>
2026-09-20 17:08:17 +0100

    Bump V2 data asset version

--- v2/assets/js/site.js
+++ v2/assets/js/site.js
@@ -1,7 +1,7 @@
 const base = '/v2/';
 const page = document.body.dataset.page;
 const params = new URLSearchParams(location.search);
-const assetVersion = '20260920-1705';
+const assetVersion = '20260920-1720';
 const load = async name => {
   const response = await fetch(`${base}assets/data/${name}.json?v=${assetVersion}`);
   if (!response.ok) throw new Error(`Cannot load ${name}: ${response.status}`);
